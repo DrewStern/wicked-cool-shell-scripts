b1f52e91f80e2db4ba2d2bacddc92bffe5c845ba
Andrew <sternandrewm@gmail.com>
2021-07-24 11:05:17 -0500

    minor update to 001

--- 001-find-programs-in-path.sh
+++ 001-find-programs-in-path.sh
@@ -2,48 +2,48 @@
 
 in_path() 
 {
-	cmd=$1
-	ourpath=$2
+	cmd=${1}
+	ourpath=${2}
 	result=1
-	oldIFS=$IFS
+	oldIFS=${IFS}
 	IFS=":"
 	
-	for directory in $ourpath
+	for directory in ${ourpath}
 	do
-		if [ -x $directory/$cmd ] ; then
+		if [ -x ${directory}/${cmd} ] ; then
 			result=0
 		fi
 	done
 
-	IFS=$oldIFS
-	return $result
+	IFS=${oldIFS}
+	return ${result}
 }
 
 checkForCmdInPath() 
 {
-	var=$1
+	var=${1}
 	
-	if [ "$var" != "" ]; then
+	if [ "${var}" != "" ]; then
 		if [ "${var:0:1}" = "/" ]; then
-			if [ ! -x $var ]; then
+			if [ ! -x ${var} ]; then
 				return 1
-			elif ! in_path $var "$PATH"; then 
+			elif ! in_path ${var} "${PATH}"; then 
 				return 2
 			fi
 		fi
 	fi
 }
 
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 command" >&2
+if [ ${#} -ne 1 ]; then
+	echo "Usage: ${0} command" >&2
 	exit 1
 fi
 
-checkForCmdInPath "$1"
-case $? in
-	0) echo "$1 found in PATH";;
-	1) echo "$1 not found or not executable";;
-	2) echo "$1 not found in PATH";;
+checkForCmdInPath "${1}"
+case ${?} in
+	0) echo "${1} found in PATH";;
+	1) echo "${1} not found or not executable";;
+	2) echo "${1} not found in PATH";;
 esac
 
 exit 0

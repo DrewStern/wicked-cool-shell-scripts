c4576311365390b83210e7fb290761863a8e542d
Andrew <sternandrewm@gmail.com>
2021-08-09 23:04:15 -0500

    VERBATIM FROM BOOK or so

---
+++ 006-validate-input-float.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+. validint
+
+validfloat() 
+{
+	if [ ! -z $(echo $fvalue | sed 's/[^.]//g') ]; then
+		decimalPart="$(echo $fvalue | cut -d. -f1)"
+		fractionalPart="${fvalue#*\.}"
+		
+		if [ ! -z $decimalPart ]; then
+			if ! validint "$decimalPart" "" ""; then 
+				return 1
+			fi
+		fi
+		
+		if [ "${fractionalPart%${fractionalPart#?}}" = "-" ]; then
+			echo "Invalid floating-point number: '-' not allowed after decimal point." >&2
+			return 1
+		fi
+		
+		if [ "$fractionalPart" != "" ]; then
+			if ! validint "$fractionalPart" "0" ""; then
+				return 1
+			fi
+		fi
+	else
+		if [ "$fvalue" = "-" ]; then 
+			echo "Invalid floating-point format." >&2
+			return 1
+		fi
+		
+		if ! validint "$fvalue" "" ""; then
+			return 1
+		fi
+	fi
+	
+	return 0
+}
